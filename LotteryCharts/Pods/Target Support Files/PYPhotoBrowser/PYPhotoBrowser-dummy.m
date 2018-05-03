73f0d2e66ccbe696c3056ca7da0f03f5f3c92ec7
linshaokai <397002535@qq.com>
2018-05-03 10:01:45 +0800

    112

--- LotteryCharts/Pods/Target Support Files/PYPhotoBrowser/PYPhotoBrowser-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PYPhotoBrowser : NSObject
-@end
-@implementation PodsDummy_PYPhotoBrowser
-@end
--- LotteryCharts/Pods/Target Support Files/Pods-LotteryCharts/Pods-LotteryCharts-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LotteryCharts : NSObject
-@end
-@implementation PodsDummy_Pods_LotteryCharts
-@end
